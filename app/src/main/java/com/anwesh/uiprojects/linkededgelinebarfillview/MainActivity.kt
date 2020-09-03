3b5c40c32349fb77b76541ff03a01b2be08c6522
Anwesh43 <anweshthecool0@gmail.com>
2020-09-04 02:15:56 +0530

    adding View in activity and fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkededgelinebarfillview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkededgelinebarfillview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkededgelinebarfillview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.edgelinebarfillview.EdgeLineBarFillView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        EdgeLineBarFillView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
