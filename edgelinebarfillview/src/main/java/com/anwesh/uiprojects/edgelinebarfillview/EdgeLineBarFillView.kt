d234250d0c18e98845926972690c98541060b625
Anwesh43 <anweshthecool0@gmail.com>
2020-09-04 01:51:07 +0530

    adding Canvas extension methods

--- edgelinebarfillview/src/main/java/com/anwesh/uiprojects/edgelinebarfillview/EdgeLineBarFillView.kt
+++ edgelinebarfillview/src/main/java/com/anwesh/uiprojects/edgelinebarfillview/EdgeLineBarFillView.kt
@@ -31,3 +31,30 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawEdgeLineBarFill(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts + 1)
+    val sf2 : Float = sf.divideScale(1, parts + 1)
+    val size : Float = w / sizeFactor
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(w / 2 - size, h / 2)
+        drawLine(0f, 0f, 0f, -h * sf1, paint)
+        drawRect(RectF(0f, -h * sf2, size, 0f), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawELBFNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawEdgeLineBarFill(scale, w, h, paint)
+}
