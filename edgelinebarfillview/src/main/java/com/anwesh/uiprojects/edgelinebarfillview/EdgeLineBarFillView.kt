856f76bc17c1a2076d1a1481354d39f18da9753b
Anwesh43 <anweshthecool0@gmail.com>
2020-09-04 01:54:10 +0530

    adding EdgeLIneBarFillView class

--- edgelinebarfillview/src/main/java/com/anwesh/uiprojects/edgelinebarfillview/EdgeLineBarFillView.kt
+++ edgelinebarfillview/src/main/java/com/anwesh/uiprojects/edgelinebarfillview/EdgeLineBarFillView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawELBFNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawEdgeLineBarFill(scale, w, h, paint)
 }
+
+class EdgeLineBarFillView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
